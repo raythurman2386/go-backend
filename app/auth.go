6412ebc0cc03b2662481fc5e5bf81bd8559a02e6
raythurman2386 <raymondthurman5@gmail.com>
2020-02-10 19:26:49 -0500

    troubleshooting error

--- app/auth.go
+++ app/auth.go
@@ -1,34 +1,37 @@
 package app
 
 import (
-	"net/http"
-	u "lens/utils"
-	"strings"
-	"go-backend/models"
-	jwt "github.com/dgrijalva/jwt-go"
-	"os"
 	"context"
 	"fmt"
+	"go-backend/models"
+	u "lens/utils"
+	"net/http"
+	"os"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 var JwtAuthentication = func(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		notAuth := []string{"/api/user/new", "/api/user/login"} // List of endpoints
-		requestPath := r.URL.requestPath
 
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		notAuth := []string{"/api/user/new", "/api/user/login"} //List of endpoints that doesn't require auth
+		requestPath := r.URL.Path                               //current request path
+
+		//check if request does not need authentication, serve the request if it doesn't need it
 		for _, value := range notAuth {
 
-			// Check if the request does not need auth, then serve the request
 			if value == requestPath {
 				next.ServeHTTP(w, r)
 				return
 			}
 		}
 
-		response := make(map[string] interface{})
-		tokenHeader := r.Header.Get("Authorization") // Grab the token from the header
+		response := make(map[string]interface{})
+		tokenHeader := r.Header.Get("Authorization") //Grab the token from the header
 
-		if tokenHeader == "" {
+		if tokenHeader == "" { //Token is missing, returns with error code 403 Unauthorized
 			response = u.Message(false, "Missing auth token")
 			w.WriteHeader(http.StatusForbidden)
 			w.Header().Add("Content-Type", "application/json")
@@ -36,19 +39,19 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 			return
 		}
 
-		splitted := strings.Split(tokenHeader, " ") // token normally comes in format `Bearer {token-body}`, we check if the token matched
+		splitted := strings.Split(tokenHeader, " ") //The token normally comes in format `Bearer {token-body}`, we check if the retrieved token matched this requirement
 		if len(splitted) != 2 {
 			response = u.Message(false, "Invalid/Malformed auth token")
-			w.WriteHeader((http.StatusForbidden))
+			w.WriteHeader(http.StatusForbidden)
 			w.Header().Add("Content-Type", "application/json")
 			u.Respond(w, response)
 			return
 		}
 
-		tokenPart := splitted[1] // grab the token part
+		tokenPart := splitted[1] //Grab the token part, what we are truly interested in
 		tk := &models.Token{}
 
-		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error {
+		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
 			return []byte(os.Getenv("token_password")), nil
 		})
 
